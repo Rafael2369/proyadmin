ee35ce52f5efcccfff91fb15e7bd9528e71d4bc9
Rafael2369 <proyplayframework@gmail.com>
2020-06-04 14:16:27 -0500

    eliminado
    error de subida por rafa

--- public/javascripts/persona.js
+++
@@ -1,9 +0,0 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-  $("#container2").load($("#PER_TIPOUSUARIO").val()); 
-});
-
-$("#PER_TIPOUSUARIO").on("click", function(){
-    let valor = $(this).val();
-    $("#container2").load(valor);
-});
